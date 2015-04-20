856123500d07e5521b11f967edf43da93a118195
James Iter <james.iter.cn@gmail.com>
2015-04-20 18:14:49 +0800

    Mini full

--- setup.py
+++ setup.py
@@ -6,36 +6,42 @@ __date__ = '15/4/20'
 __contact__ = 'james.iter.cn@gmail.com'
 __copyright__ = '(c) 2015 by James Iter.'
 
+import codecs
+import os
+
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return codecs.open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
 PACKAGE = "jimit"
 NAME = "jimit"
 DESCRIPTION = "James Iter's common library by python."
 AUTHOR = "James Iter"
 AUTHOR_EMAIL = "james.iter.cn@gmail.com"
-URL = ""
+URL = "https://github.com/jamesiter/jimitlib-py.git"
 VERSION = __import__(PACKAGE).__version__
 
 setup(
     name=NAME,
     version=VERSION,
     description=DESCRIPTION,
-    long_description=read("README.rst"),
+    long_description=read("README.md"),
     author=AUTHOR,
     author_email=AUTHOR_EMAIL,
     license="BSD",
     url=URL,
     packages=find_packages(exclude=["tests.*", "tests"]),
-    package_data=find_package_data(
-        PACKAGE,
-        only_in_packages=False
-    ),
     classifiers=[
-        "Development Status :: 3 - Alpha",
-        "Environment :: Web Environment",
+        "Development Status :: 1 - Alpha",
+        "Environment :: Service Environment",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
+        "Operating System :: Linux",
         "Programming Language :: Python",
-        "Framework :: Django",
+        "Framework :: None",
         ],
     zip_safe=False,
 )
