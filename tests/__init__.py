4c13f81ea8dcd90ed4f9d75fbc84c045b534c8f1
James Iter <james.iter.cn@gmail.com>
2015-05-17 01:47:32 +0800

    实现项整体测试

--- tests/__init__.py
+++ tests/__init__.py
@@ -5,3 +5,14 @@ __author__ = 'James Iter'
 __date__ = '15/4/25'
 __contact__ = 'james.iter.cn@gmail.com'
 __copyright__ = '(c) 2015 by James Iter.'
+
+
+import os
+
+
+def load_tests(loader, standard_tests, pattern='test_*.py'):
+    # top level directory cached on loader instance
+    this_dir = os.path.dirname(__file__)
+    package_tests = loader.discover(start_dir=this_dir, pattern=pattern)
+    standard_tests.addTests(package_tests)
+    return standard_tests
---
+++ tests/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'James Iter'
+__date__ = '15/5/17'
+__contact__ = 'james.iter.cn@gmail.com'
+__copyright__ = '(c) 2015 by James Iter.'
+
+import tests
+import unittest
+
+
+suite = tests.load_tests(unittest.TestLoader(), unittest.TestSuite())
+unittest.TextTestRunner(verbosity=2).run(suite)
